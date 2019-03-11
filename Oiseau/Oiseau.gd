6b004106f5dd8dde89020e1294ddf643868605c0
Atelier3D <cogrel.t@stjosta.net>
2019-03-11 16:59:29 +0100

    mouvement joueur

--- Oiseau/Oiseau.gd
+++ Oiseau/Oiseau.gd
@@ -1,13 +1,20 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (int) var vitesse = 100
+export (int) var saut = 70
 
-# Called when the node enters the scene tree for the first time.
+var mouvement = Vector2(vitesse, 0)
+
+const GRAVITE = 50
+
+
 func _ready():
-	pass # Replace with function body.
+	pass 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	if Input.is_action_just_pressed("saut"):
+		mouvement.y -= saut
+
+	mouvement.y += GRAVITE * delta
+	
+	mouvement = move_and_slide(mouvement)
